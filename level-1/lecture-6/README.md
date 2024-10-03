bb4bd3e8a096d976d9a2aa2dfaadfb7137bacd91
thenandita <149591077+thenandita@users.noreply.github.com>
2024-10-03 23:20:21 +0600

    added type casting

---
+++ level-1/lecture-6/README.md
@@ -0,0 +1,5 @@
+## type casting
+
+### resources
+
+* https://www.w3schools.com/python/python_casting.asp
---
+++ level-1/lecture-6/type_casting.py
@@ -0,0 +1,25 @@
+a = int(3)  # output :3
+b = int(4.6) # output :4
+c = int('6') # output :6
+
+print(a)
+print(b)
+print(c)
+
+a = float(2)   # output 2.0
+b = float(3.7) # output 3.7
+c = float("6") # output 6.0
+d = float("4.4") # output 4.4
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+a = str("city5") # output 'city5'
+b = str(2) # output: '2'
+c = str(5.0) # output: '5.0'
+
+print(a)
+print(b)
+print(c)
