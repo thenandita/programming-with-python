b9c9f3a35d173b412d513349ad3f8dee841f6c28
thenandita <149591077+thenandita@users.noreply.github.com>
2024-10-04 18:49:32 +0600

    added loops

---
+++ level-1/lecture-5/README.md
@@ -0,0 +1,5 @@
+## loops
+
+### resources
+
+* https://www.w3schools.com/python/python_for_loops.asp
---
+++ level-1/lecture-5/loops.py
@@ -0,0 +1,45 @@
+a = 3
+while a < 6:
+    print(a)
+    a += 3
+
+a = 2
+while a < 10:
+    print(a)
+    if a == 4:
+        break
+    a += 2
+
+a = 1
+while a < 7:
+    a += 2
+    if a == 3:
+        continue
+    print(a)
+
+veg = ["corn", "cauliflower", "tomato", "capsicum", "spinach"]
+for pick in veg:
+    print(pick)
+    if pick == "tomato":
+        break
+
+veg = ["corn", "cauliflower", "tomato", "capsicum", "spinach"]
+for pick in veg:
+    if pick == "tomato":
+        break
+    print(pick)
+
+for value in range(3, 7):
+    print(value)
+
+for value in range(3, 29, 3):
+    print(value)
+
+food = ["chicken", "mutton", "beef"]
+dishes = ["curry", "steak", "kebab"]
+
+for meats in food:
+  for items in dishes:
+    print(meats, items)
+
+
